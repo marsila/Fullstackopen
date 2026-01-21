46042b32ab2cf9bc21208177f65833dec8e125ac
marsila <eng.rasha.ali7@gmail.com>
2026-01-21 14:38:48 +0100

    unicafe step2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,45 +1,49 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const Button = (props) => {
-  return(
-    <button onClick={props.handleClick}>{props.text}</button>
-  )
-}
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
 function App() {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-const handleGoodClick =() => {
-  console.log('good clicked');
-  setGood(prev => prev+1);  
-}
+  const all = good + neutral + bad;
+  const avarage = (good - bad)/all || 0;
+  const positive = (good/all)*100 || 0;
 
-const handleNeutralClick =() => {
-  console.log('neutral clicked');
-  setNeutral(prev => prev+1);  
-}
+  const handleGoodClick = () => {
+    console.log("good clicked");
+    setGood((prev) => prev + 1);
+  };
 
-const handleBadClick =() => {
-  console.log('bad clicked');
-  setBad(prev => prev+1);
-}
+  const handleNeutralClick = () => {
+    console.log("neutral clicked");
+    setNeutral((prev) => prev + 1);
+  };
 
+  const handleBadClick = () => {
+    console.log("bad clicked");
+    setBad((prev) => prev + 1);
+  };
 
   return (
     <>
-     <h1>Give feedback</h1>
-     <Button text={"good"} handleClick={handleGoodClick}/>
-     <Button text={"neutral"} handleClick={handleNeutralClick}/>
-     <Button text={"bad"} handleClick={handleBadClick}/>
-     <div>
-      <h1>Statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-     </div>
+      <h1>Give feedback</h1>
+      <Button text={"good"} handleClick={handleGoodClick} />
+      <Button text={"neutral"} handleClick={handleNeutralClick} />
+      <Button text={"bad"} handleClick={handleBadClick} />
+      <div>
+        <h1>Statistics</h1>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {all}</p>
+        <p>average {avarage}</p>
+        <p>positive {positive} %</p>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
