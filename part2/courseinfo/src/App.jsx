6e4a81c1ced9890d576840d2c2c5334932bc9be7
marsila <eng.rasha.ali7@gmail.com>
2026-01-25 13:51:34 +0100

    exercies 2.1: Course information step 6

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,10 +1,58 @@
+const Header = ({ name }) => {
+  return <h1>{name}</h1>;
+};
+
+const Part = ({name, exercises}) => {
+  return (
+    <p>
+      {name}  {exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <ul>
+      {parts.map(part => 
+        <Part name={part.name} key={part.id} exercises={part.exercises} />
+      )}
+    </ul>
+  )
+};
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </>
+  );
+};
 
 function App() {
-  
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
 
-  return (
-    <h1>Part2</h1>
-  );
+  return <Course course={course} />;
 }
 
 export default App;
