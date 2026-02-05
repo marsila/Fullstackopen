148b01efc52b6750eeb8b01455c4e3d7c94bdbe2
marsila <eng.rasha.ali7@gmail.com>
2026-02-05 20:50:21 +0100

    first commit

---
+++ part2/data-for-countries/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './index.css'
+function App() {
+  
+  return (
+    <>
+      <h1>Countries App</h1>
+      <form>
+        <div>
+          <label htmlFor="findCountry">find countries </label>
+          <input type="text" name="findCountry" />
+        </div>
+      </form>      
+    </>
+  )
+}
+
+export default App
