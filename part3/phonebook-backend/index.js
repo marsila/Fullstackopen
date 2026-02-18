931708d908edbce55c3cb18c16832ef387452382
marsila <eng.rasha.ali7@gmail.com>
2026-02-18 11:20:22 +0100

    exercise 3.7: Phonebook backend step 7

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const PORT = 3001;
 
@@ -26,6 +27,7 @@ let persons = [
 ];
 
 app.use(express.json());
+app.use(morgan('tiny'))
 
 app.get('/', (request, response) => {
     response.send('<h1>PhoneBook App</h1>');
