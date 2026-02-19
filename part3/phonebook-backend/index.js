9cf3f01c3a7fb6e2a2fc202cc770820f6b59b82a
marsila <eng.rasha.ali7@gmail.com>
2026-02-19 12:53:02 +0100

    exercise 3.9 Phonebook backend step 9

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -28,6 +28,8 @@ let persons = [
 
 app.use(express.json());
 
+app.use(express.static('dist'))
+
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
