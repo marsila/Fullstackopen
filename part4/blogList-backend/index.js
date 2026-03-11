9b6675be8763a468abff6762afa8357d941fe808
marsila <eng.rasha.ali7@gmail.com>
2026-03-11 16:13:47 +0100

    Refactor the application into separate modules

--- part4/blogList-backend/index.js
+++ part4/blogList-backend/index.js
@@ -1,31 +1,7 @@
+const app = require('./app') // the actual Express application
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const express = require('express')
-const Blog = require('./models/blog')
 
-const app = express();
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
-
-app.get('/',(req,res)=>{
-    res.send('<h1>The Blog List</h1>')
-})
-
-
-app.listen(config.PORT,()=> {logger.info(`The server is open on the port ${config.PORT}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
--- part4/blogList-backend/models/blog.js
+++ part4/blogList-backend/models/blog.js
@@ -1,16 +1,4 @@
 const mongoose = require('mongoose')
-const config = require('../utils/config')
-const logger = require('../utils/logger')
-
-
-
-mongoose.connect(config.MONGODB_URI, { family: 4 })
-.then(() => {
-    logger.info('Connected successfully!')
-  })
-  .catch(err => {
-    logger.error('Connection error:', err.message)
-  })
 
 const blogSchema = new mongoose.Schema({
   title: {
