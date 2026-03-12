eb1c392eab521079f6851f8dcd5bdb78574c4280
marsila <eng.rasha.ali7@gmail.com>
2026-03-12 10:06:03 +0100

    Deleting a blog and test it with vs code REST

--- part4/blogList-backend/controllers/blogs.js
+++ part4/blogList-backend/controllers/blogs.js
@@ -21,14 +21,34 @@ blogsRouter.get('/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog.save().then((result) => {
-        response.status(201).json(result)
+blogsRouter.post('/', (request, response, next) => {
+  const body = request.body
+  if (!body.title || !body.author || !body.url) {
+    return response.status(400).json({
+      error: 'content missing'
     })
+  }
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url:body.url,
+    likes:0
+  })
+  blog.save()
+    .then(savedBlog => {
+      response.json(savedBlog)
+      console.log('added new blog ', blog)
+    })
+    .catch(error => next(error))
 })
 
+
+
+blogsRouter.delete('/:id', (request,response, next) => {
+  Blog.findByIdAndDelete(request.params.id)
+    .then(() => response.status(204).end())
+    .catch(error => next(error))
+})
 
 
 
