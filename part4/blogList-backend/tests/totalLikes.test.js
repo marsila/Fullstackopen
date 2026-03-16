c425d19638f138f128d3ead6800165d1c1641a35
marsila <eng.rasha.ali7@gmail.com>
2026-03-16 13:23:24 +0100

    exercise 4.4: Helper Functions and Unit Tests, step 2

---
+++ part4/blogList-backend/tests/totalLikes.test.js
@@ -0,0 +1,51 @@
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
+const listHelper = require('../utils/list_helper')
+
+describe('total likes', () => {
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 5,
+            __v: 0
+        }
+    ]
+
+    test('when list has only one blog, equals the likes of that', () => {
+        const result = listHelper.totalLikes(listWithOneBlog)
+        assert.strictEqual(result, 5)
+    })
+
+
+    const listWithMoreBlogs = [
+        {
+            "title": "My first blog",
+            "author": "Anna Morch",
+            "url": "http://localhost:3003",
+            "likes": 3,
+            "id": "69afe37fca75e5621bb0c78d"
+        },
+        {
+            "title": "this is a second update for the  blog",
+            "author": "Marta Scott",
+            "url": "http://localhost:3003",
+            "likes": 6,
+            "id": "69b188c83743fdb143b22d1d"
+        },
+        {
+            "title": "this is a second blog",
+            "author": "Liana Maria",
+            "url": "http://localhost:3003",
+            "likes": 0,
+            "id": "69b2850a4446e908b0c5215b"
+        }
+    ]
+
+    test('lists with more than one blog', ()=>{
+        const result = listHelper.totalLikes(listWithMoreBlogs)
+        assert.strictEqual(result,9)
+    })
+})
