c291df0d8276b624c1b73759c104cd4557dbc608
marsila <eng.rasha.ali7@gmail.com>
2026-03-22 13:55:59 +0100

    exercise 4.8 step 1

---
+++ part4/blogList-backend/tests/blog_api.test.js
@@ -0,0 +1,16 @@
+const {test, after} = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+test('get all the blogs', async() => {
+    await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+after(async()=>{
+    await mongoose.connection.close()
+})
