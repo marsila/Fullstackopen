bfce6f2bf98578e3f08aad1a265ec1a992f4f372
marsila <eng.rasha.ali7@gmail.com>
2026-04-07 16:18:12 +0200

    exercise 4.15: Blog List Expansion, step 3

--- part4/blogList-backend/controllers/users.js
+++ part4/blogList-backend/controllers/users.js
@@ -1,9 +1,8 @@
 const bcrypt = require('bcrypt')
-const usersRouter =require('express').Router
+const usersRouter =require('express').Router()
 const User = require('../models/user')
-const log = require('../utils/logger')
 
-usersRouter.post('/api/users',async(request,response)=> {
+usersRouter.post('/',async(request,response)=> {
     const {username,name,password}=request.body
     const passwordHash = await bcrypt.hash(password,10)
 
@@ -11,6 +10,11 @@ usersRouter.post('/api/users',async(request,response)=> {
 
     const savedUser = await user.save()
     response.status(201).json(savedUser)
+})
+
+usersRouter.get('/',async(request,response) => {
+    const users = await User.find({})
+    response.json(users)
 })
 
 module.exports = usersRouter
