bad4fa8e94a54a213d582b3d8887988cea4923bd
marsila <eng.rasha.ali7@gmail.com>
2026-04-11 10:30:37 +0200

    add token authentication for execise 4.18 , rename the folder request to requests

--- part4/blogList-backend/controllers/blogs.js
+++ part4/blogList-backend/controllers/blogs.js
@@ -2,6 +2,8 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const logger = require('../utils/logger')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const config  = require('../utils/config')
 
 
 blogsRouter.get('/', async (request, response) => {
@@ -18,18 +20,33 @@ blogsRouter.get('/:id', async (request, response) => {
   }
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  const user = await User.findById(body.userId)
-  console.log('user',user);
-  
+
+  const decodedToken = jwt.verify(getTokenFrom(request),config.SECRET_TOKEN)
+
+  if(!decodedToken){
+    return response.status(401).json({error:'invalid token'})
+  }
+
+  const user = await User.findById(decodedToken.id)
+  console.log('user', user);
+
   if (!body.title || !body.url) {
     return response.status(400).json({
       error: 'title or url missing'
     })
   }
 
-  if(!user){
+  if (!user) {
     return response.status(400).json({
       error: 'UserId missing or not valid'
     })
@@ -39,7 +56,7 @@ blogsRouter.post('/', async (request, response) => {
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
-    user:user._id
+    user: user._id
   })
 
   const savedBlog = await blog.save()
--- part4/blogList-backend/controllers/login.js
+++ part4/blogList-backend/controllers/login.js
@@ -5,11 +5,15 @@ const User = require('../models/user')
 const config =require('../utils/config')
 
 loginRouter.post('/', async (request, response) => {
+
     const { username, password } = request.body
+
     const user = await User.findOne({ username })
-    const passwordCorrect = user === nul
+
+    const passwordCorrect = user === null
         ? false
         : await bcrypt.compare(password, user.passwordHash)
+
     if (!(user && passwordCorrect)) {
         return response.status(401).json({ error: 'invalid username or password' })
     }
