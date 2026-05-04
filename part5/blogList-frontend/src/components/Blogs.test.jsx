54f00cca0013ae7198b16e7141c6b77a4738e173
marsila <eng.rasha.ali7@gmail.com>
2026-05-04 12:29:40 +0200

    complete exercise 5.14: Blog List Tests, step 2

--- part5/blogList-frontend/src/components/Blogs.test.jsx
+++ part5/blogList-frontend/src/components/Blogs.test.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable no-undef */
 import {render,screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('first test', () => {
@@ -10,5 +12,25 @@ test('first test', () => {
   //I have 2 title in the page (hide/show)
   const div = container.querySelector('.blogDetails')
   expect(div).toHaveTextContent('the test')
+
+})
+
+test('hide and show butten', async()=>{
+  const blog ={
+    title:'button test',
+    author:'admin',
+    likes:4,
+    url:'admin.blogs.com'
+  }
+  const mockHandler = vi.fn()
+  render(<Blog blog={blog} toggleBlogDetails={mockHandler}/>)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  screen.debug(button)
+  await user.click(button)
+  const element1 = screen.getByText('admin.blogs.com',{exact:false})
+  const element2 = screen.getByText('likes: 4',{exact:false})
+  expect(element1,element2).toBeDefined()
 
 })
