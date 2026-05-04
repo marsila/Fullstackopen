e7ae3a7c459f1fa47d9e3b4e05eb7b607dbc6f41
marsila <eng.rasha.ali7@gmail.com>
2026-05-04 13:05:53 +0200

    minor fixes and comlete exercise

--- part5/blogList-frontend/src/components/Blogs.test.jsx
+++ part5/blogList-frontend/src/components/Blogs.test.jsx
@@ -15,7 +15,7 @@ test('first test', () => {
 
 })
 
-test('hide and show butten', async()=>{
+test('view/hide details button', async()=>{
   const blog ={
     title:'button test',
     author:'admin',
@@ -27,10 +27,40 @@ test('hide and show butten', async()=>{
 
   const user = userEvent.setup()
   const button = screen.getByText('view')
-  screen.debug(button)
   await user.click(button)
   const element1 = screen.getByText('admin.blogs.com',{exact:false})
   const element2 = screen.getByText('likes: 4',{exact:false})
   expect(element1,element2).toBeDefined()
 
 })
+
+test('cliking like button twice, calling the click handler twice', async () => {
+  const blog = {
+    title:'like button test',
+    author:'admin',
+    url: 'admin.blogs.com',
+    likes:0,
+    user:{
+      id:'69e0b618b4fc66a6a6d443d4',
+      username:'admin'
+    },
+    id:'fakeblog1'
+  }
+
+  const mockHandler = vi.fn()
+  render(<Blog
+    blog={blog}
+    updateBlogLikes={mockHandler}
+    loggedUser='admin'
+  />)
+
+  const user = userEvent.setup()
+  const viewButton = screen.getByText('view')
+  await user.click(viewButton)
+  const likeButton = screen.getByText('like')
+  await user.click(likeButton)
+  await user.click(likeButton)
+
+  expect(mockHandler.mock.calls).toHaveLength(2)
+})
+
