593946d95b20c794630dbd893e1704499db97b39
marsila <eng.rasha.ali7@gmail.com>
2026-05-15 13:57:31 +0200

    completed exercise 6.5: anecdotes, step4

--- part6/zustand-anecdotes/src/components/AnecdoteList.jsx
+++ part6/zustand-anecdotes/src/components/AnecdoteList.jsx
@@ -1,24 +1,26 @@
-import { useAnecdotes } from "../store"
-import { useAnecdoteActions } from "../store"
+import { useAnecdotes } from "../store";
+import { useAnecdoteActions } from "../store";
 
 const AnecdoteList = () => {
-  const anecdotes = useAnecdotes()
-  const { increaseVotes } = useAnecdoteActions()
+  const anecdotes = useAnecdotes();
+  const { increaseVotes } = useAnecdoteActions();
 
   const vote = (id) => {
-    increaseVotes(id)
-  }
+    increaseVotes(id);
+  };
   return (
     <>
-      {anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
+      {anecdotes
+        .toSorted((a, b) => b.votes - a.votes)
+        .map((anecdote) => (
+          <div key={anecdote.id}>
+            <div>{anecdote.content}</div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => vote(anecdote.id)}>vote</button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </>
   );
 };
